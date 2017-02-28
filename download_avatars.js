4ae721478d4201f2995106fcc35add826f1ff329
anthonylgrant <anthonylorenzogrant@gmail.com>
2017-02-27 18:18:03 -0800

    successfully requested api endpoint using token, logged output to console

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,18 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "anthonylgrant";
-var GITHUB_TOKEN = "4155c0cf39d11eb20ab1b3bd4b884aca927e4f49node";
+var GITHUB_TOKEN = "4155c0cf39d11eb20ab1b3bd4b884aca927e4f49";
 var repoOwner = "jquery";
 var repoName = "jquery";
 var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`
-
+var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'request'
+  },
+  json: true
+};
 console.log(`RequestURL: ${requestURL}`);
 console.log('Welcome to the Github Avatar Downloader');
 
@@ -16,4 +23,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors:", err);
   console.log("Result:", result);
+});
+
+request(options, function(err, response, body) {
+  if (err) throw err;
+  console.log("Body:",body);
+  console.log('Response Status Code:', response.statusCode);
 });
