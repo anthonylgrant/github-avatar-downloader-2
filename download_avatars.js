c41bf6437313e7f6f2bd23b105d93d8d17fe6eb2
anthonylgrant <anthonylorenzogrant@gmail.com>
2017-02-27 19:23:03 -0800

    finished assignment, need to revisit proper usage of options object

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,6 @@ var GITHUB_USER = "anthonylgrant";
 var GITHUB_TOKEN = "4155c0cf39d11eb20ab1b3bd4b884aca927e4f49";
 var repoOwner = "jquery";
 var repoName = "jquery";
-var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`
 var options = {
   url: requestURL,
   headers: {
@@ -13,22 +12,72 @@ var options = {
   },
   json: true
 };
-console.log(`RequestURL: ${requestURL}`);
-console.log('Welcome to the Github Avatar Downloader');
+var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'request'
+  },
+  json: true
+};
+// var repoUser = process.argv[2];
+// var repoName = process.argv[3];
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(cb) {
 
+  // if (!repoName || !repoUser) {
+  //   console.error("Please provide a valid Github Username and Repo");
+  //   return;
+  // }
+
+  request(options, (err, response, body) => {
+    if (err) {
+      return cb(err);
+    };
+    console.log('Response Status Code:', response.statusCode);
+    console.log("Response Status Message: ", response.statusMessage);
+    console.log("Response Content Type: ", response.headers['content-type']);
+    cb(null, body); // allows to pass body without needing to parse as JSOON -- provides array with objects
+    // if response.body was callbacked, then JSON parse would be needed
+  })
+};
+
+function downloadImageByUrl(url, filePath) {
+  console.log("URL:", url);
+  request.get(url, (err, response, body) => {
+    if (err) throw err;
+    console.log('Response Status Code:', response.statusCode);
+  })
+  .on('response', (response) => {
+    console.log('Response Status Code:', response.statusCode);
+    console.log("Response Status Message: ", response.statusMessage);
+    console.log("Response Content Type: ", response.headers['content-type']);
+    console.log("Downloading image ...");
+    console.log("Download complete.");
+  })
+  .pipe(fs.createWriteStream(`./avatar-photos/${filePath}.jpg`));
 }
 
-getRepoContributors("jquery", "jquery", function(err, result){
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
-
-request(options, function(err, response, body) {
+getRepoContributors((err, result) => {
   if (err) throw err;
-  body.forEach((obj) => {
-    console.log(obj.avatar_url);
+  result.forEach((user) => {
+    console.log(user.avatar_url);
+    downloadImageByUrl(user.avatar_url, user.login)
   })
-  console.log('Response Status Code:', response.statusCode);
-});
+})
+
+// downloadImageByUrl(options, "./avatar_images.");
+
+// getRepoContributors("jquery", "jquery", function(err, result){
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+//   console.log("YOOYOOYOOYO");
+// });
+
+// request(options, function(err, response, body) {
+//   if (err) throw err;
+//   body.forEach((obj) => {
+//     console.log(obj.avatar_url);
+//   })
+//   console.log('Response Status Code:', response.statusCode);
+// });
